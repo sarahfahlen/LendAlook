4174e6da2b065aa80d0414dbef46847bf5384106
sarahfahlen <sarah.fahlen@live.dk>
2025-04-04 11:42:42 +0200

    interface

--- LALapp/Services/IClosetService.cs
+++ LALapp/Services/IClosetService.cs
@@ -2,11 +2,11 @@ using Core;
 namespace LALapp;
 public interface IClosetService
 {
-    Task<tøj> GetAll();
-    Task AddItem();
-    Task DeleteById();
-    Task UpdateItem();
-    Task BookItem();
-    Task FilterBy();
+    Task<tøj[]> GetAll();
+    Task AddItem(tøj item);
+    Task DeleteById(int id);
+    Task UpdateItem(tøj item);
+    Task BookItem(int id, DateOnly slutDato);
+    Task FilterBy(string? type, string? farve, string? størrelse, bruger? udlåner, bool? ledig);
 
 }
