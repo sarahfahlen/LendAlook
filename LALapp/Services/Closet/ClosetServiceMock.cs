4452e3e4fd83d36973693d5bf073fd2f9baae582
sarahfahlen <sarah.fahlen@live.dk>
2025-04-07 10:25:20 +0200

    plz virk

--- LALapp/Services/Closet/ClosetServiceMock.cs
+++ LALapp/Services/Closet/ClosetServiceMock.cs
@@ -1,58 +1,59 @@
 using Core;
+using static LALapp.Services.LoginServiceClientSide;
 namespace LALapp;
 
 public class ClosetServiceMock : IClosetService
 {
-   public static tøj test1 = new tøj { id = 1, type = "bluse", størrelse = "small", beskrivelse = "efe", farve = "blå"};
-   public static tøj test2 = new tøj { id = 2, type = "bukser", størrelse = "medium", beskrivelse = "rth", farve = "sort"};
+    public static tøj test1 = new tøj { id = 1, type = "bluse", størrelse = "small", beskrivelse = "efe", farve = "blå", ejer = rip};
+    public static tøj test2 = new tøj { id = 2, type = "bukser", størrelse = "medium", beskrivelse = "rth", farve = "sort", ejer = rap};
    
-   public List<tøj> altTøj = new List<tøj>{test1, test2};
+    public List<tøj> altTøj = new List<tøj>{test1, test2};
 
 
-   public async Task<tøj[]> GetAll()
-   {
-      return altTøj.ToArray();
-   }
+    public async Task<tøj[]> GetAll()
+    {
+        return altTøj.ToArray();
+    }
 
-   public async Task AddItem(tøj item)
-   {
-      int max = 0;
-      if (altTøj.Count > 0)
-      {
-         max = altTøj.Select(t => t.id).Max();
-      }
-      item.id = max + 1;
-      altTøj.Add(item);
-   }
+    public async Task AddItem(tøj item)
+    {
+        int max = 0;
+        if (altTøj.Count > 0)
+        {
+            max = altTøj.Select(t => t.id).Max();
+        }
+        item.id = max + 1;
+        altTøj.Add(item);
+    }
    
-   public async Task DeleteById(int id)
-   {
-      altTøj.RemoveAll(t => t.id == id);
-   }
+    public async Task DeleteById(int id)
+    {
+        altTøj.RemoveAll(t => t.id == id);
+    }
 
-   public async Task UpdateItem(tøj item)
-   {
-      var index = altTøj.FindIndex(t => t.id == item.id); //finder det objekt hvor ID matcher, og tjekker dets index (plads)
-      if (index != -1) //hvis index =-1 betyder det at objektet ikke findes - derfor tjekkes der her om der er et match
-      {
-         altTøj[index] = item; //hvis der er match, laves det objekt vi fandt om til det vi sender med (det opdaterede)
-      }
+    public async Task UpdateItem(tøj item)
+    {
+        var index = altTøj.FindIndex(t => t.id == item.id); //finder det objekt hvor ID matcher, og tjekker dets index (plads)
+        if (index != -1) //hvis index =-1 betyder det at objektet ikke findes - derfor tjekkes der her om der er et match
+        {
+            altTøj[index] = item; //hvis der er match, laves det objekt vi fandt om til det vi sender med (det opdaterede)
+        }
       
-   }
+    }
 
-   public async Task BookItem(tøj item, DateOnly slut)
-   {
-      tøj bookingItem = altTøj.FirstOrDefault(t => t.id == item.id);
-      if (bookingItem != null)
-      {
-         bookingItem.reserveret = item.reserveret; //sætter status for det valgte item, til den status der ændres på page (vigtigt vi ændrer på page)
-         bookingItem.slutDato = slut; //skal sendes fra siden også
-      }
-   }
+    public async Task BookItem(tøj item, DateOnly slut)
+    {
+        tøj bookingItem = altTøj.FirstOrDefault(t => t.id == item.id);
+        if (bookingItem != null)
+        {
+            bookingItem.reserveret = item.reserveret; //sætter status for det valgte item, til den status der ændres på page (vigtigt vi ændrer på page)
+            bookingItem.slutDato = slut; //skal sendes fra siden også
+        }
+    }
 
-   public Task FilterBy(string? type, string? farve, string? størrelse, bruger? udlåner, bool? ledig)
-   {
-      throw new NotImplementedException();
-   }
+    public Task FilterBy(string? type, string? farve, string? størrelse, bruger? udlåner, bool? ledig)
+    {
+        throw new NotImplementedException();
+    }
    
 }
