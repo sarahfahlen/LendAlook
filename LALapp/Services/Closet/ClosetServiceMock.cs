ef5ba2b46e1347a0d28eabc252f753f4fd829272
sarahfahlen <sarah.fahlen@live.dk>
2025-04-07 12:47:43 +0200

    slet af ligegyldige sider

--- LALapp/Services/Closet/ClosetServiceMock.cs
+++ LALapp/Services/Closet/ClosetServiceMock.cs
@@ -5,7 +5,7 @@ namespace LALapp;
 public class ClosetServiceMock : IClosetService
 {
     public static tøj test1 = new tøj { id = 1, type = "Overdele", størrelse = "Small", farve = "Navy", mærke = "H&M", beskrivelse = "Fin bluse fra H&M, med broderede ærmer", img = "https://images1.vinted.net/t/04_01117_ecEptDFqCNGYznpBeuLxkaCa/f800/1744020601.jpeg?s=9d2a74e4a3264d6657f2f4eeb9869c7e96f66b7e", ejer = rip};
-    public static tøj test2 = new tøj { id = 2, type = "Bukser", størrelse = "38", beskrivelse = "Løse jeans fra Weekday", farve = "Blå", mærke = "Weekday", img = "https://images1.vinted.net/t/02_024f5_kxM8t9whpBSSU9FJaqVuVY5X/f800/1743883988.jpeg?s=9ecf658925ba03703ecdee21e11cc3937528e442", ejer = rap};
+    public static tøj test2 = new tøj { id = 2, type = "Bukser", størrelse = "Medium", beskrivelse = "Løse jeans fra Weekday", farve = "Blå", mærke = "Weekday", img = "https://images1.vinted.net/t/02_024f5_kxM8t9whpBSSU9FJaqVuVY5X/f800/1743883988.jpeg?s=9ecf658925ba03703ecdee21e11cc3937528e442", ejer = rap};
    
     public List<tøj> altTøj = new List<tøj>{test1, test2};
 
